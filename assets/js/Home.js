f28f1cea4fe260406e8c36e35d351fc7fff7b620
valdiev <valdiev@live.fr>
2022-04-11 21:48:45 +0200

    Adding home into an external file

---
+++ assets/js/Home.js
@@ -0,0 +1,45 @@
+import { carapuce } from "./Pokemolle.js";
+import { bulbizar } from "./Pokemolle.js";
+import { salameche } from "./Pokemolle.js";
+
+import main from "./Main.js";
+
+export default function choosePoke() {
+
+    let map = document.querySelector('.screen');
+
+
+    map.innerHTML = `
+        <section class="choosePoke">
+            <div class="character" data-pokemolle="${carapuce.nom}">
+                <div class="character__image"><img src="../assets/img/Pokemolle/${carapuce.image}" alt=""></div>
+                <h2>${carapuce.nom}</h2>
+            </div>
+            <div class="character" data-pokemolle="${bulbizar.nom}">
+                <div class="character__image"><img src="../assets/img/Pokemolle/${bulbizar.image}" alt="""></div>
+                <h2>${bulbizar.nom}</h2>
+            </div>
+            <div class="character" data-pokemolle="${salameche.nom}">
+            <div class="character__image"><img src="../assets/img/Pokemolle/${salameche.image}" alt=""></div>
+                <h2>${salameche.nom}</h2>
+            </div>
+        </section>
+    `
+
+    let characters = document.querySelectorAll('.choosePoke .character');
+
+
+    // DETECT POKEMOLLE AND PUT OBJECT IN ARRAY
+    characters.forEach(element => {
+        element.addEventListener('click', () => {
+            const pokePicked = element.getAttribute('data-pokemolle');
+            if (pokePicked === "Carapuce") {
+                main(carapuce);
+            } else if (pokePicked === "Bulbizar") {
+                main(bulbizar);
+            } else {
+                main(salameche);
+            }
+        })
+    });
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,13 @@
-import { carapuce } from "./Pokemolle.js";
-import { bulbizar } from "./Pokemolle.js";
-import { salameche } from "./Pokemolle.js";
-
 import { ennemolles } from "./Ennemolle.js";
+import choosePoke from "./Home.js";
 
 let map = document.querySelector('.screen');
 let round = 0;
 
-function choosePoke() {
-
-    map.innerHTML = `
-        <section class="choosePoke">
-            <div class="character" data-pokemolle="${carapuce.nom}">
-                <div class="character__image"><img src="../assets/img/Pokemolle/${carapuce.image}" alt=""></div>
-                <h2>${carapuce.nom}</h2>
-            </div>
-            <div class="character" data-pokemolle="${bulbizar.nom}">
-                <div class="character__image"><img src="../assets/img/Pokemolle/${bulbizar.image}" alt="""></div>
-                <h2>${bulbizar.nom}</h2>
-            </div>
-            <div class="character" data-pokemolle="${salameche.nom}">
-            <div class="character__image"><img src="../assets/img/Pokemolle/${salameche.image}" alt=""></div>
-                <h2>${salameche.nom}</h2>
-            </div>
-        </section>
-    `
-
-    let characters = document.querySelectorAll('.choosePoke .character');
+choosePoke();
 
 
-    // DETECT POKEMOLLE AND PUT OBJECT IN ARRAY
-    characters.forEach(element => {
-        element.addEventListener('click', () => {
-            const pokePicked = element.getAttribute('data-pokemolle');
-            if (pokePicked === "Carapuce") {
-                main(carapuce);
-            } else if (pokePicked === "Bulbizar") {
-                main(bulbizar);
-            } else {
-                main(salameche);
-            }
-        })
-    });
-}
-
-
-function main(pokemolleChosen) {
+export default function main(pokemolleChosen) {
     let pokemolle = pokemolleChosen;
     console.log(ennemolles);
     let ennemolle = ennemolles[0];
@@ -213,4 +175,3 @@ function main(pokemolleChosen) {
         }
     }
 }
-choosePoke();
